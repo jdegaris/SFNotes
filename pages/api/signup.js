967e8f6aa5fea6ccfc1b690e2126490699727515
jdegaris <j.a.degaris@gmail.com>
2020-05-16 19:58:44 -0400

    trying to deploy to heroku

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,6 +1,5 @@
 import connectDb from '../../utils/connectDb'
 import User from '../../models/User'
-import Cart from '../../models/Cart'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import isEmail from 'validator/lib/isEmail'
