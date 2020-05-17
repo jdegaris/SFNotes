df8d9b6b370b8709764a65e4e37cd8271b4c9537
jdegaris <j.a.degaris@gmail.com>
2020-05-16 20:29:00 -0400

    trying to deploy to heroku again

---
+++ next.config.js
@@ -0,0 +1,7 @@
+// must restart server whenever you make changes in next.config
+module.exports = {
+  env: {
+    MONGO_SRV: "mongodb+srv://james:SalesforceNotes@sfnotes-xacnk.mongodb.net/sfnotes?retryWrites=true&w=majority",
+    JWT_SECRET: "salesforcedevelopersftw"
+  }
+};
