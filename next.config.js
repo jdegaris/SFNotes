697ab481534a33cf2ee67e6e4bab2d8b5c258f06
James <j.a.degaris@gmail.com>
2020-05-16 23:07:27 -0400

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-// must restart server whenever you make changes in next.config
-module.exports = {
-  env: {
-    MONGO_URI: "mongodb+srv://james:SalesforceNotes@sfnotes-xacnk.mongodb.net/sfnotes?retryWrites=true&w=majority",
-    JWT_SECRET: "salesforcedevelopersftw"
-  }
-};
