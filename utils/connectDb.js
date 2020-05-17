b08015a9588820aecc2bb362e51b4a2c0bd34f59
jdegaris <j.a.degaris@gmail.com>
2020-05-16 22:54:14 -0400

    trying heroku again

--- utils/connectDb.js
+++ utils/connectDb.js
@@ -2,20 +2,33 @@ import mongoose from 'mongoose'
 const connection = {}
 
 async function connectDb() {
-    if (connection.isConnected) {
-        // Use existing database connection
-        console.log('Using existing connection');
-        return;
+    // if (connection.isConnected) {
+    //     // Use existing database connection
+    //     console.log('Using existing connection');
+    //     return;
+    // }
+    // // Use new database connection
+    // const db = await mongoose.connect(process.env.MONGO_URI, {
+    //     useCreateIndex: true,
+    //     useFindAndModify: false,
+    //     useNewUrlParser: true,
+    //     useUnifiedTopology: true
+    // })
+    // console.log("DB Connected");
+    // connection.isConnected = db.connections[0].readyState
+
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useCreateIndex: true,
+            useFindAndModify: false,
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        console.log('DB Connected');
+    } catch (err) {
+        console.error(err.message)
+        process.exit(1)
     }
-    // Use new database connection
-    const db = await mongoose.connect(process.env.MONGO_URI, {
-        useCreateIndex: true,
-        useFindAndModify: false,
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    console.log("DB Connected");
-    connection.isConnected = db.connections[0].readyState
 }
 
 export default connectDb
